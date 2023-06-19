1e31cb5911e15b3f8908e8673f2b8250063aa3d2
jhembe <jhembe202@gmail.com>
2023-06-19 11:41:00 +0300

    done with v1 of root file cusomisation

---
+++ v1/scripts/app.js
@@ -0,0 +1,63 @@
+$(document).ready(function () {
+    var isScrolling;
+
+    function hideNavbar() {
+        $('.navbar').addClass('hidden');
+    }
+
+    function showNavbar() {
+        $('.navbar').removeClass('hidden');
+    }
+
+    // Parallax effect on scroll
+    $(window).scroll(function () {
+        clearTimeout(isScrolling);
+        hideNavbar();
+
+        isScrolling = setTimeout(function () {
+            showNavbar();
+        }, 100);
+    });
+
+    // Parallax effect on scroll
+    $(window).scroll(function () {
+        var scrollTop = $(this).scrollTop();
+        $('.hero-section').css('background-position-y', -(scrollTop / 2) + 'px');
+    });
+    $(window).scroll(function () {
+        var scrollTop = $(this).scrollTop();
+        $('.hero-section').css('transform', 'translate3d(0, ' + -(scrollTop * 0.5) + 'px, 0)');
+    });
+
+    // Smooth scrolling to target sections
+    $('.nav-link').click(function (e) {
+        e.preventDefault();
+        var target = $(this).attr('href');
+        $('html, body').animate({
+            scrollTop: $(target).offset().top
+        }, 800);
+    });
+
+    // Show/hide scroll-to-top button
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 100) {
+            $('.scroll-to-top').addClass('show');
+        } else {
+            $('.scroll-to-top').removeClass('show');
+        }
+    });
+
+    // Scroll to top on button click
+    $('.scroll-to-top').click(function () {
+        $('html, body').animate({
+            scrollTop: 0
+        }, 800);
+        return false;
+    });
+
+    // Hamburger menu toggle
+    $('.navbar-toggler').click(function () {
+        $(this).toggleClass('open');
+        $('.navbar-collapse').slideToggle();
+    });
+});
